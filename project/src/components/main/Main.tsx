f81a3b5d3fb2fbd11cee9d26c86f200a267acf60
Mish_Sanek <kid02@list.ru>
2022-02-11 11:34:10 +0500

    Разделил все страницы на компоненты. Удалил тесты. Прокинул пропсы для главной страницы

---
+++ project/src/components/main/Main.tsx
@@ -0,0 +1,49 @@
+import { ICardProps } from '../../types/CardTypes';
+import Header from '../header/header';
+import MainEmpty from '../mainEmpty/mainEmpty';
+import PlaceCard from '../placeCard/PlaceCard';
+import Sort from '../sort/sort';
+import Tabs from '../tabs/tabs';
+
+interface CardProps {
+  placeCardsData: ICardProps[];
+}
+
+function Main({placeCardsData}: CardProps): JSX.Element {
+
+  return (
+    <div className="page page--gray page--main">
+      <Header />
+      <main className={`page__main page__main--index ${!placeCardsData.length && 'page__main--index-empty'}`}>
+        <h1 className="visually-hidden">Cities</h1>
+        <Tabs />
+        <div className="cities">
+          <div className={`cities__places-container ${!placeCardsData.length && 'cities__places-container--empty'} container`}>
+            {
+              placeCardsData.length ?
+                <>
+                  <section className="cities__places places">
+                    <h2 className="visually-hidden">Places</h2>
+                    <b className="places__found">{placeCardsData.length} places to stay in Amsterdam</b>
+                    <Sort />
+                    <div className="cities__places-list places__list tabs__content">
+                      {
+                        placeCardsData.map((card: ICardProps) => <PlaceCard key={card.id} card={card} />)
+                      }
+                    </div>
+                  </section>
+                  <div className="cities__right-section">
+                    <section className="cities__map map" />
+                  </div>
+                </>
+                :
+                <MainEmpty />
+            }
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Main;
