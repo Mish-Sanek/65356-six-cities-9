0e962e7c082727a3b5b8981b06eb484ecef96839
Mish_Sanek <kid02@list.ru>
2022-02-20 21:00:45 +0500

    Улучшения

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import {Route, Routes, useLocation } from 'react-router-dom';
 import { ICardProps } from '../../types/CardTypes';
 import Favorites from '../favorites/favorites';
+import Header from '../header/header';
 import Login from '../login/login';
 import Main from '../main/Main';
 import NotFound from '../notFound/notFound';
+import PrivateRoute from '../privateRoute/PrivateRoute';
 import Room from '../room/room';
 
 interface CardProps {
@@ -12,16 +15,36 @@ interface CardProps {
 
 function App({placeCardsData}: CardProps): JSX.Element {
 
+  const location = useLocation();
+
+  const [locationState, setLocationState] = useState('/');
+
+  useEffect(() => {
+    setLocationState(location.pathname);
+  }, [locationState]);
+
+
   return (
-    <BrowserRouter>
+    <div className={`
+      page
+      ${locationState === '/' && 'page--gray page--main'}
+      ${locationState === '/login' && 'page--gray page--login'}
+    `}
+    >
+      <Header locationState={locationState} />
       <Routes>
         <Route path="/" element={<Main placeCardsData={placeCardsData} />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/favorites" element={<Favorites />} />
         <Route path="/offer/" element={<Room />} />
+        <Route path="/favorites" element={
+          <PrivateRoute>
+            <Favorites />
+          </PrivateRoute>
+        }
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
